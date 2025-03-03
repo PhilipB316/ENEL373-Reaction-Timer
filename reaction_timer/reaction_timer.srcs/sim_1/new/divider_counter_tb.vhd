ce7b4ed5b6bce8372b69b79452feccb1456e01f4
Michael Brown <mbr206@uclive.ac.nz>
2025-03-03 15:47:07 +1300

    Add clock divider, encoder, and decoder testbench

---
+++ reaction_timer/reaction_timer.srcs/sim_1/new/divider_counter_tb.vhd
@@ -0,0 +1,61 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03.03.2025 15:32:57
+-- Design Name: 
+-- Module Name: divider_counter_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity divider_counter_tb is
+--  Port ( );
+end divider_counter_tb;
+
+architecture Behavioral of divider_counter_tb is
+    signal CLK : STD_LOGIC := '0';
+    signal SLOWCLK : STD_LOGIC := '0';
+    signal UPPERBOUND : STD_LOGIC_VECTOR (27 downto 0) := X"0000004";
+    signal ENCODED_3B : STD_LOGIC_VECTOR (2 downto 0) := "000";
+    signal DECODED_3B : STD_LOGIC_VECTOR (7 downto 0) := X"00";
+    
+begin
+    inst_clock_divider : entity work.clk_divider(Behavioral)
+    port map (CLK100MHZ_IN => CLK, UPPERBOUND_IN => UPPERBOUND, SLOWCLK_OUT => SLOWCLK);
+    
+    inst_counter_3b : entity work.counter_3b(Behavioral)
+    port map (CLK_IN => CLK, COUNT_OUT => ENCODED_3B);
+    
+    inst_decoder_3b : entity work.decoder_3b(Behavioral)
+    port map (ENC_IN => ENCODED_3B, ENC_OUT => DECODED_3B);
+    
+simulation : process
+begin
+    CLK <= not CLK;
+    wait for 10ns;
+end process;
+
+end Behavioral;
