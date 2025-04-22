c5451d59213060d6dce7808b6f0d0cb22c5dbbd0
Michael Brown <mbr206@uclive.ac.nz>
2025-04-22 14:59:25 +1200

    Add 8-digit binary-to-bcd module using double-dabble algorithm

---
+++ reaction_timer/reaction_timer.srcs/sim_1/new/binary_to_bcd_8_tb.vhd
@@ -0,0 +1,87 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+ENTITY binary_to_bcd_8_tb IS
+END binary_to_bcd_8_tb;
+ 
+ARCHITECTURE behavior OF binary_to_bcd_8_tb IS
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT binary_to_bcd_8
+    PORT(
+         clk : IN  std_logic;
+         reset : IN  std_logic;
+         binary_in : IN  std_logic_vector(27 downto 0);
+         bcd0 : OUT  std_logic_vector(3 downto 0);
+         bcd1 : OUT  std_logic_vector(3 downto 0);
+         bcd2 : OUT  std_logic_vector(3 downto 0);
+         bcd3 : OUT  std_logic_vector(3 downto 0);
+         bcd4 : OUT  std_logic_vector(3 downto 0);
+         bcd5 : OUT  std_logic_vector(3 downto 0);
+         bcd6 : OUT  std_logic_vector(3 downto 0);
+         bcd7 : OUT  std_logic_vector(3 downto 0)
+        );
+    END COMPONENT;
+ 
+   --Inputs
+   signal clk : std_logic := '0';
+   signal reset : std_logic := '0';
+   signal binary_in : std_logic_vector(27 downto 0) := (others => '0');
+ 
+    --Outputs
+   signal bcd0 : std_logic_vector(3 downto 0);
+   signal bcd1 : std_logic_vector(3 downto 0);
+   signal bcd2 : std_logic_vector(3 downto 0);
+   signal bcd3 : std_logic_vector(3 downto 0);
+   signal bcd4 : std_logic_vector(3 downto 0);
+   signal bcd5 : std_logic_vector(3 downto 0);
+   signal bcd6 : std_logic_vector(3 downto 0);
+   signal bcd7 : std_logic_vector(3 downto 0);
+ 
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
+ 
+BEGIN
+ 
+    -- Instantiate the Unit Under Test (UUT)
+   uut: binary_to_bcd_8 PORT MAP (
+          clk => clk,
+          reset => reset,
+          binary_in => binary_in,
+          bcd0 => bcd0,
+          bcd1 => bcd1,
+          bcd2 => bcd2,
+          bcd3 => bcd3,
+          bcd4 => bcd4,
+          bcd5 => bcd5,
+          bcd6 => bcd6,
+          bcd7 => bcd7
+        );
+ 
+   -- Clock process definitions
+   clk_process :process
+   begin
+        clk <= '0';
+        wait for clk_period/2;
+        clk <= '1';
+        wait for clk_period/2;
+   end process;
+ 
+   -- Stimulus process
+   stim_proc: process
+   begin
+      -- hold reset state for 100 ns.
+        reset <= '1';
+      wait for 100 ns;
+        reset <= '0';
+ 
+        binary_in <= X"000000F";
+      wait for 200 ns;
+ 
+        binary_in <= X"0BC614E";
+      wait for 2000 ns;
+ 
+    end process;
+ 
+END;
