c0dd7289ae50bd769671e9a346bced57e3035a61
Michael Brown <mbr206@uclive.ac.nz>
2025-05-07 13:36:51 +1200

    Update mux comment to match new 5-bit bcd size

--- reaction_timer/reaction_timer.srcs/sources_1/new/main.vhd
+++ reaction_timer/reaction_timer.srcs/sources_1/new/main.vhd
@@ -282,7 +282,7 @@ begin
                                        SELECT_IN => output_segment_select,
                                        MUX_OUT => encoded_error_text); 
 
---  8x4 to 4 encoded display data mux
+--  8x5 to 5 encoded display data mux
     ff11: multiplexer_8_1_4b port map (MUX_IN_0 => encoded_reaction_time_digit,
                                        MUX_IN_1 => selected_alu_bcd_digit,
                                        MUX_IN_2 => selected_alu_bcd_digit,
